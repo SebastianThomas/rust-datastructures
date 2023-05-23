57ce96604d3dac4608369449fd1e93a608fb7947
Sebastian Thomas <mail@sthomas.ch>
2023-05-23 20:01:05 +0200

    Added LinkedList-Node implementation

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::rc::Rc;
+
+fn main() {
+    println!("Hello, world!");
+    let mut node1: Node<i32> = Node::new(5);
+    println!("{:?}", node1);
+    {
+        let node2: Node<i32> = Node::new_from_rc(&node1.value());
+        println!("{:?}", node2);
+        node1.set_next_node(node2);
+    }
+    println!("{:?}", node1);
+    println!("{:?}", node1.get_next_node());
+    println!("{:?}", node1.next_node);
+    
+}
+
+#[derive(Debug)]
+struct Node<T> {
+    value: Rc<T>,
+    next_node: Option<Box<Node<T>>>,
+}
+
+impl<T> Node<T> {
+    fn new(v: T) -> Node<T> {
+        Node { value: Rc::new(v), next_node: None }
+    }
+
+    fn new_from_rc(vrc: &Rc<T>) -> Node<T> {
+        Node { value: vrc.clone(), next_node: None }
+    }
+
+    fn value(&self) -> Rc<T> {
+       self.value.clone()
+    }
+
+    fn set_next_node(&mut self, node: Node<T>) {
+        self.next_node = Some(Box::new(node));
+    }
+
+    fn get_next_node(&self) -> &Option<Box<Node<T>>> {
+        &self.next_node
+    }
+}
